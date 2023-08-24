ed833cc6a76882bdb195c302482c9186dccac70e
Dan Morton <dancmorton@gmail.com>
2023-08-24 17:05:52 -0400

    modify webhook

--- netlify/functions/polarisWebook.js
+++ netlify/functions/polarisWebook.js
@@ -1,19 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const dataFilePath = path.join(__dirname, '..', 'data', 'webhookData.json');
+
 exports.handler = async function (event, context) {
-    try {
-      const payload = JSON.parse(event.body);
-  
-      // You can now work with the payload received from GitLab.
-      // Process the payload as needed for your use case.
-  
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Webhook received successfully' }),
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ message: 'An error occurred' }),
-      };
-    }
-  };
-  
+  try {
+    const payload = JSON.parse(event.body);
+
+    // Store the payload in the JSON file
+    const existingData = fs.existsSync(dataFilePath)
+      ? JSON.parse(fs.readFileSync(dataFilePath, 'utf8'))
+      : [];
+    existingData.push(payload);
+    fs.writeFileSync(dataFilePath, JSON.stringify(existingData, null, 2));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Webhook received and data stored' }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'An error occurred' }),
+    };
+  }
+};
